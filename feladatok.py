6a22419c1ff0f3cc97072626c4a57b7b5dfca7aa
Frnc36 <magoriferenc19@gmail.com>
2025-10-21 14:25:03 +0200

    elso_feladat() kesz

--- feladatok.py
+++ feladatok.py
@@ -1,1 +1,18 @@
-print("helo")
+""" 
+1. Mennyi a programozás jegyek átlaga? 
+
+2. Összesen hány ötös van?  
+"""
+
+
+def elso_feladat(lista = []):
+    print(lista)
+    i = 0
+    cv = 0
+    osszesen=0
+    while cv < len(lista):
+        osszesen+=lista[i]
+        cv+=1
+        i+=1
+    atlag=osszesen//len(lista)
+    print(atlag)
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 import feladatok
+
+lista = [5,2,3,4,4,5,5,5,1,2,2,3,4,5,5,5,4,3,3,]
+
+feladatok.elso_feladat(lista)
