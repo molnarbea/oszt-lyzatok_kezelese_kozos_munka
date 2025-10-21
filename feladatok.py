ac73ca57dd6a8371200ee7d77bb295df7bf99fe8
molnarbea <molnarbeatrix11@gmail.com>
2025-10-21 14:17:50 +0200

    harmadik feladat kesz

--- feladatok.py
+++ feladatok.py
@@ -1,1 +1,10 @@
-print("helo")
+def harmadik_feladat(lista=[]):
+    cv=0
+    i=0
+    elegtelen=0
+    while len(lista) > cv:
+        if lista[i] == 1:
+            elegtelen+=1
+        i+=1
+        cv+=1
+    print(f"Összesen {elegtelen} diák kaputt elégtelent.")
